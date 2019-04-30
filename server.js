3b78525999ffa7930305721be5d99bd7f49d209e
admin <sangram@chainflux.com>
2019-04-30 18:20:08 +0530

    https added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var app = require('./app');
 var https = require('https');
 var fs = require('fs');
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8003;
 
 var server = app.listen(port, function() {
   console.log('Express server listening on port ' + port);
